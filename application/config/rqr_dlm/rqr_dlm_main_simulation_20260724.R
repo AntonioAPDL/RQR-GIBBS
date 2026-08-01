281802e9a51f4e739bdb9b5211bbea5d1fd8ba29
Antonio <antonio.agprz@gmail.com>
2026-08-01 13:37:40 -0700

    Authorize RQR-DLM confirmatory simulation

--- application/config/rqr_dlm/rqr_dlm_main_simulation_20260724.R
+++ application/config/rqr_dlm/rqr_dlm_main_simulation_20260724.R
@@ -135,7 +135,7 @@ rqr_dlm_main_simulation <- list(
     mcmc_transition_and_fixed_role_schedule_changed = TRUE
   ),
   diagnostic_pilot_execution_authorized = FALSE,
-  confirmatory_execution_authorized = FALSE,
+  confirmatory_execution_authorized = TRUE,
   implemented_modes = c(
     "preflight", "oracle-reference", "sentinel-core",
     "execute-confirmatory", "collect", "audit"
